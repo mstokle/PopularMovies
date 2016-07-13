29ddf81fadc38cf6d479d57c32b47caf3a29fba5
Stokle <Mariano_Stokle@amer.dell.com>
2016-07-13 14:30:57 -0300

    Initial commit

---
+++ app/src/main/java/ar/com/stokle/popularmovies/PopMoviesActivityFragment.java
@@ -0,0 +1,22 @@
+package ar.com.stokle.popularmovies;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PopMoviesActivityFragment extends Fragment {
+
+    public PopMoviesActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pop_movies, container, false);
+    }
+}
